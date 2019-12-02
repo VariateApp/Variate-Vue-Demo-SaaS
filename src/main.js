87c519820d2278ccf58a57feaf232db76e90b3ee
alhankeser <alhankeser@gmail.com>
2019-12-01 21:56:39 -0500

    Initialize the plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
 
+import config from '../variate.json';
+import Variate from '@variate/vue';
+
+Vue.use(Variate, {
+  debug: true,
+  tracking: true,
+  config,
+});
+
 import router from './router'
 
 Vue.config.productionTip = false
